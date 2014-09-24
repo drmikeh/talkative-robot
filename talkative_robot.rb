d1ff7a652112e88a5745b6e7b177747e8f965542
Michael Hopper <mike.hopper@ga.co>
2014-09-24 09:58:25 -0400

    The robot asks the user some questions.

--- talkative_robot.rb
+++ talkative_robot.rb
@@ -1,1 +1,9 @@
 puts "Greetings, Human!"
+
+puts "What is your name? "
+user_name = gets.chomp
+
+puts "What is your age? "
+user_age = gets.chomp.to_i
+
+puts "Hi #{user_name} who is #{user_age} years old!"
