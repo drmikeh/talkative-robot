809139586ee43254af91074e3c88eaa0be126fdb
Michael Hopper <mike.hopper@ga.co>
2014-09-24 10:13:34 -0400

    Added more questions, some basic math, and string interpolation.

--- talkative_robot.rb
+++ talkative_robot.rb
@@ -7,3 +7,10 @@ puts "What is your age? "
 user_age = gets.chomp.to_i
 
 puts "Hi #{user_name} who is #{user_age} years old!"
+
+years = 75 - user_age
+puts "You will be 75 years old in #{years} years."
+
+user_name.upcase
+puts "Hey #{user_name}, where are you going!?"
+puts %s(Yo "Dude", what's up?)
