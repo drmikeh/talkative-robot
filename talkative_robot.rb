f6bb89643dd2e27dea34ffd483cdc1a4190a7ee0
Michael Hopper <mike.hopper@ga.co>
2014-09-24 10:16:42 -0400

    Added some control flow using while loop and if/else.

--- talkative_robot.rb
+++ talkative_robot.rb
@@ -8,6 +8,19 @@ user_age = gets.chomp.to_i
 
 puts "Hi #{user_name} who is #{user_age} years old!"
 
+first_initial = user_name[0]
+
+user_answer = ""
+while user_answer != "yes" && user_answer != "no"
+    puts "Do you mind if I call you #{first_initial} (yes or no)?"
+    user_answer = gets.chomp.downcase
+end
+if user_answer == "yes"
+    puts "Okay, I will not call you that."
+else
+    puts "Cool!"
+end
+
 years = 75 - user_age
 puts "You will be 75 years old in #{years} years."
 
